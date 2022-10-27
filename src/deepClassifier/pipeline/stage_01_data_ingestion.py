ce1fef58344cc597f0ce25a6fb4998dbf6c38e80
Shreeyashp <shreepardeshi1999@gmail.com>
2022-10-27 20:39:28 +0530

    format document

--- src/deepClassifier/pipeline/stage_01_data_ingestion.py
+++ src/deepClassifier/pipeline/stage_01_data_ingestion.py
@@ -4,12 +4,14 @@ from deepClassifier import logger
 
 STAGE_NAME = "Data Ingestion stage"
 
+
 def main():
     config = ConfigurationManager()
     data_ingestion_config = config.get_data_ingestion_config()
     data_ingestion = DataIngestion(config=data_ingestion_config)
     data_ingestion.download_file()
     data_ingestion.unzip_and_clean()
+
 
 if __name__ == '__main__':
     try:
@@ -18,4 +20,4 @@ if __name__ == '__main__':
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.exception(e)
-        raise e 
+        raise e
